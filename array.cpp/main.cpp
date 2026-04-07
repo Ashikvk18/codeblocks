4ab9ec1462f485c79eeaf95a0b92e1753b10b996
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-07 15:07:58 -0500

    size of

--- array.cpp/main.cpp
+++ array.cpp/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main()
 {
     int a[5]={1,2,4};
+    a[4]= 7;
+    cout << "size of this array " << sizeof(a) << endl;
     cout << "differently using for loop" << endl;
     for(int i = 0; i < 5; i++){
         cout << a[i] << endl;
