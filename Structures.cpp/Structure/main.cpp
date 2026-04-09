7a19e1089a85629c959f797fb0f3bfdb96582e0c
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-09 10:42:20 -0500

    .

--- Structures.cpp/Structure/main.cpp
+++ Structures.cpp/Structure/main.cpp
@@ -10,11 +10,14 @@ struct Rectangle
 
 int main()
 {
-   struct Rectangle r;
-   r.w = 5;
-   r.l = 10;
-   cout << "The area of rectangle r is : " << r.l * r.w << endl;
-   struct Rectangle x = {15,10};
-   cout << "The area of rectangle x is : " << x.l * x.w << endl;
-    return 0;
+       struct Rectangle r;
+       r.w = 5;
+       r.l = 10;
+       cout << "The area of rectangle r is : " << r.l * r.w << endl;
+       struct Rectangle x = {15,10};
+       cout << "The area of rectangle x is : " << x.l * x.w << endl;
+       x.w = 20;
+       x.l = 10;
+       cout << "The area of rectangle x is : " << x.l * x.w << endl;
+       return 0;
 }
