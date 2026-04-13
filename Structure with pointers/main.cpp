c4e29a1e50597e2ecde898e11133366445e6dca0
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-12 23:10:47 -0500

    streak

--- Structure with pointers/main.cpp
+++ Structure with pointers/main.cpp
@@ -11,5 +11,6 @@ int main()
 {
     struct Student s1 = {18, "Ashik"};
     cout << "the size of this struct is : " << sizeof(s1) << endl; // concept of padding that's why it is not 22 it is 24. Struct takes the multiple of the largest data type.
+    cout << endl;
     return 0;
 }
