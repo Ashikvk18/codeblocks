12ec7680b503660fea1220e66b6a2915693e77ba
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-21 13:09:26 -0500

    unique ptr

--- pointers/main.cpp
+++ pointers/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
@@ -11,6 +12,15 @@ int main()
 {
     int (*f) (int, int) = add;
     cout << f(5, 6) << endl;
+
+    unique_ptr<int> p1 = make_unique<int>(10);
+    cout << *p1 << endl;
+    cout << &p1;
+
+
+
+
+
     system("pause>0");
     return 0;
 }
