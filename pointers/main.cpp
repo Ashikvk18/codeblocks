c8eb5159f346195f20af6db8866193bd1c8ecfc4
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-21 13:12:25 -0500

    unique ptr

--- pointers/main.cpp
+++ pointers/main.cpp
@@ -15,7 +15,11 @@ int main()
 
     unique_ptr<int> p1 = make_unique<int>(10);
     cout << *p1 << endl;
-    cout << &p1;
+    cout << &p1 << endl;
+
+    unique_ptr<int> p2 = move(p1);
+    cout << *p2 << endl;
+    cout << *p1;
 
 
 
