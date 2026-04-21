985ce4cacfaa5b1ebfcbcdbc18829f23eabd30a8
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-21 12:59:50 -0500

    pointers

---
+++ pointers/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int (*f) (int, int) = add;
+    cout << f(5, 6) << endl;
+    system("pause>0");
+    return 0;
+}
